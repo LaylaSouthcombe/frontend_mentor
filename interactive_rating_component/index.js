42013c8337e173c125465e6f3c1401da6f924adf
LaylaSouthcombe <laylasouthcombe789@outlook.com>
2022-09-14 16:46:09 +0100

    add active states

--- interactive_rating_component/index.js
+++ interactive_rating_component/index.js
@@ -2,8 +2,17 @@ const submitBtn = document.getElementById("submitBtn")
 const ratingCard = document.querySelector(".ratingCard")
 const thankYouCard = document.querySelector(".thankYouCard")
 
+const inputs = document.querySelectorAll('input[name="ratingNum"]')
+
+inputs.forEach(x => {
+    x.addEventListener("click", (e) => {
+        e.target.parentElement.classList.add("activeInput")
+    })
+})
+
 const submitFeedback = () => {
     const checkValue = document.querySelector('input[name="ratingNum"]:checked')
+    console.log(checkValue.parentElement)
     const feedBackString = document.getElementById("feedBackString")
     feedBackString.textContent = `You selected ${checkValue.value} out of 5`
     ratingCard.style.display = "none"
