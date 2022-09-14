a1e9cc19ca599a5fd7db3c24ecc6e9c312f9c69f
LaylaSouthcombe <laylasouthcombe789@outlook.com>
2022-09-14 18:58:39 +0100

    add function to not allow numbers in text input field

---
+++ interactive_card_details_form/index.js
@@ -0,0 +1,39 @@
+const cardNumOutput = document.querySelector(".cardNumOutput")
+const cardNameOutput = document.querySelector(".cardNameOutput")
+const cardExpDateOutput = document.querySelector(".cardExpDateOutput")
+const cardCVCOutput = document.querySelector(".cardCVCOutput")
+
+const cardNumberInput = document.getElementById("cardNumberInput")
+
+cardNumberInput.addEventListener("keypress", () => {
+    let index = cardNumberInput.value.lastIndexOf(' ');
+    let testStr = cardNumberInput.value.substr(index + 1);
+    if (testStr.length === 4 && (cardNumberInput.value.length !== 19)) {
+        cardNumberInput.value = cardNumberInput.value + ' ';
+    } 
+})
+
+const inputs = document.querySelectorAll('input[type="text"]')
+const numberInputs = [].slice.call(inputs, 1)
+
+var reg = new RegExp('^[0-9]+$');
+numberInputs.forEach(x => {
+    x.addEventListener("input", (e) => {
+        if(!reg.test(e.target.value)){
+            e.target.value = e.target.value.slice(0, -1)
+        }
+    })
+})
+
+const form = document.querySelector("form")
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const data = new FormData(e.target);
+    // console.log([...data.entries()]);
+    const inputArray = [...data.entries()]
+    cardNumOutput.textContent = inputArray[0][1]
+    cardNameOutput.textContent = inputArray[1][1]
+    cardExpDateOutput.textContent = inputArray[2][1]
+    cardCVCOutput.textContent = inputArray[3][1]
+})
